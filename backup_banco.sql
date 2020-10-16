4608076c846e2ecedfb5d8407f7d84e17d2d7b51
Bonieky Lacerda <suporte@b7web.com.br>
2020-10-15 23:17:02 -0300

    aula Estrutura MVC Autoload  Core 1

--- backup_banco.sql
+++
@@ -1,4 +0,0 @@
-dfdfdfkldjf
-djfkdjkfjkdjfd
-dljfdjkfjdk
-jkjkfjkkkkkkkkkk
---
+++ estrutura_mvc/controllers/homeController.php
@@ -0,0 +1,10 @@
+<?php
+class homeController {
+
+	public function index() {
+		echo 'Olá Mundo!';	}
+
+		public function teste() {
+			echo "Este é um teste!";	}
+}
+?>
